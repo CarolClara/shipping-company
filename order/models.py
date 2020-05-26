02a27f6fef4e0468c87947e490be41f33c8fc74e
Caroline Clara <carolineclaradsl@gmail.com>
2020-05-26 11:18:58 -0300

    Altera o nome padrão dos campos

--- order/models.py
+++ order/models.py
@@ -4,14 +4,15 @@ from register.models import Address, User
 
 
 class Merchandise(models.Model):
-    height = models.FloatField()
-    width = models.FloatField()
-    weight = models.FloatField()
-    value = models.DecimalField(decimal_places=2, max_digits=20)
+    height = models.FloatField(verbose_name='Altura')
+    width = models.FloatField(verbose_name='Largura')
+    weight = models.FloatField(verbose_name='Volume')
+    value = models.DecimalField(decimal_places=2, max_digits=20, verbose_name='Valor')
 
 
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
-    origin = models.ForeignKey(Address, on_delete=models.PROTECT, related_name='order_origin')
-    destiny = models.ForeignKey(Address, on_delete=models.PROTECT, related_name='order_destiny')
-    merchandises = models.ManyToManyField(Merchandise)
+    user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='Cliente')
+    origin = models.ForeignKey(Address, on_delete=models.PROTECT, related_name='order_origin', verbose_name='Origem')
+    destiny = models.ForeignKey(Address, on_delete=models.PROTECT, related_name='order_destiny', verbose_name='Destino')
+    date = models.DateField(verbose_name='Data')
+    merchandises = models.ManyToManyField(Merchandise, verbose_name='Mercadoria')
--- register/models.py
+++ register/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 
 class Address(models.Model):
-    street = models.CharField(max_length=255)
-    city = models.CharField(max_length=255)
-    state = models.CharField(max_length=255)
-    country = models.CharField(max_length=255)
-    zip_code = models.CharField(max_length=20)
+    street = models.CharField(max_length=255, verbose_name='Rua')
+    city = models.CharField(max_length=255, verbose_name='Cidade')
+    state = models.CharField(max_length=255, verbose_name='Estado')
+    country = models.CharField(max_length=255, verbose_name='País')
+    zip_code = models.CharField(max_length=20, verbose_name='CEP')
 
 
 class User(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField()
-    cnpj = models.CharField(max_length=255)
-    password = models.CharField(max_length=50)
-    address = models.ForeignKey(Address, on_delete=models.PROTECT)
+    name = models.CharField(max_length=255, verbose_name='Nome')
+    email = models.EmailField(verbose_name='Email')
+    cnpj = models.CharField(max_length=255, verbose_name='CNPJ')
+    password = models.CharField(max_length=50, verbose_name='Senha')
+    address = models.ForeignKey(Address, on_delete=models.PROTECT, verbose_name='Endereço')
