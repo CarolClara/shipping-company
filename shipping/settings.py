e5967057b291823d0bc4200ff769123e41e7cb29
Caroline Clara <carolineclaradsl@gmail.com>
2020-06-01 23:10:54 -0300

    Altera BD

--- shipping/settings.py
+++ shipping/settings.py
@@ -57,8 +57,10 @@ WSGI_APPLICATION = 'shipping.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gotta-go-fast',
+        'USER': 'carolclara',
+        'PASSWORD': '_GottaGoFast_456'
     }
 }
 
